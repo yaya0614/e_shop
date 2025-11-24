8be1258a77c722a7d946297d5a281719ab82b4e8
ZHANG,MENG-QI <cccyr06140425@gmail.com>
2025-11-24 10:35:45 +0800

    chore: configure tailwind and shadcn in nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import { defineNuxtConfig } from "nuxt/config";
+import { defineNuxtConfig } from 'nuxt/config';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineNuxtConfig({
-  compatibilityDate: "2025-07-15",
+  compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ["nuxt-typed-router", "@nuxt/eslint"],
+  modules: ['nuxt-typed-router', '@nuxt/eslint', 'shadcn-nuxt'],
+  css: ['~/assets/css/tailwind.css'],
+  shadcn: {
+    prefix: '',
+    componentDir: '@/components/ui',
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
